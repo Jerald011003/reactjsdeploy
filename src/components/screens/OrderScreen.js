19acd0ef612cfe7860a0e2352a5d60be73db315d
coninegaming <coninegaming@gmail.com>
2023-03-27 22:25:58 +0800

    first commit

--- src/components/screens/OrderScreen.js
+++ src/components/screens/OrderScreen.js
@@ -1,9 +1,9 @@
-
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { addToCart } from '../../actions/cartActions'
 
 function OrderScreen({ match, location, history }) {
     const productId = match.params.id
